c2da0daf1b66e556ad31f6ee47636d0de9b80fce
Vasu-23085 <vasuku23085@gmail.com>
2026-09-16 14:08:21 +0530

    Add files via upload

---
+++ CircularDependency/src/main/java/in/strike/PaymentService.java
@@ -0,0 +1,20 @@
+package in.strike;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentService {
+    @Autowired
+    private OrderService orderService;
+
+
+//    public PaymentService(OrderService orderService){
+//        this.orderService = orderService;
+//    }
+    public void pay(){
+        System.out.println("Payment done.");
+
+        orderService.getOrderDetails();
+    }
+}
