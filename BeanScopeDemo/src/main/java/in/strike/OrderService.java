5d1a296beb51805aaeb055c304a9c45287ceaaf4
Vasu-23085 <vasuku23085@gmail.com>
2026-09-17 22:29:27 +0530

    Add files via upload

---
+++ BeanScopeDemo/src/main/java/in/strike/OrderService.java
@@ -0,0 +1,17 @@
+package in.strike;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("singleton")
+public class OrderService {
+
+    public OrderService(){
+        System.out.println("Order Service Created.");
+    }
+
+    public void placeOrder(){
+        System.out.println("Order Places");
+    }
+}
